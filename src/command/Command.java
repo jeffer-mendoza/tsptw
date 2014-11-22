0f90dc2b1df42f33cf3eb9a78476d9ea5b4be35c
jefferson mendoza <mendosajefferson@gmail.com>
2014-11-22 17:52:56 -0500

    Subiendo modificaciones

--- src/command/Command.java
+++ src/command/Command.java
@@ -28,13 +28,11 @@ public class Command {
         FileReader fr = new FileReader(fileInput);
         BufferedReader br = new BufferedReader(fr);
 
-
-
         String linea;
         int n = 0;//total de nodos
         int count = 0;//variable que cuenta el numero de ciclos
-        int nodos[][] = null;
-        int distancias[][] = null;
+        float nodos[][] = null;
+        float distancias[][] = null;
         int i;
         String celdas[] = null;//variable temporal para guardas los datos de la n-esima linea
         try {
@@ -43,26 +41,31 @@ public class Command {
                     break;
                 }
                 if (count == 0) {//leer solo la primera linea
+                    linea = linea.replace("\\ ", "");
                     n = Integer.parseInt(linea);//asignar el numero de notos
-                    nodos = new int[n][3];
-                    distancias = new int[n * (n - 1)][n * (n - 1)];
+                    nodos = new float[n][3];
+                    distancias = new float[n * (n - 1)][n * (n - 1)];
                     count++;
                     continue;
                 }
                 if (count < n + 1) {
                     celdas = linea.split("\\ ");//orden:{i, ts, ai, bi}
-                    i = Integer.parseInt(celdas[0]);//nodo n-esimo
-                    nodos[i][0] = Integer.parseInt(celdas[1]);//primer columna  - ts
-                    nodos[i][1] = Integer.parseInt(celdas[2]);//segunda columna - ai
-                    nodos[i][2] = Integer.parseInt(celdas[3]);//tercer columna -bi
+                    i = Integer.parseInt(celdas[0]) - 1;//nodo n-esimo
+                    nodos[i][0] = Float.parseFloat(celdas[1]);//primer columna  - ts
+                    nodos[i][1] = Float.parseFloat(celdas[2]);//segunda columna - ai
+                    nodos[i][2] = Float.parseFloat(celdas[3]);//tercer columna -bi
                 } else {
                     celdas = linea.split("\\ ");//orden:{i, j, tdij}
-                    distancias[Integer.parseInt(celdas[0])][Integer.parseInt(celdas[1])] = Integer.parseInt(celdas[2]);
+                    distancias[Integer.parseInt(celdas[0]) - 1][Integer.parseInt(celdas[1]) - 1] = Float.parseFloat(celdas[2]);
                 }
                 count++;
             }
-            generatorLP(n, distancias, nodos);//generar el archivo lp 
 
+            generatorLP(n, distancias, nodos);//generar el archivo lp 
+            lp_solve(n);
+
+        } catch (IOException e) {
+            e.printStackTrace();
         } catch (Exception e) {
             e.printStackTrace();
         } finally {
@@ -86,16 +89,17 @@ public class Command {
      * @param nodos
      * @throws IOException
      */
-    public static void generatorLP(int n, int distancias[][], int nodos[][]) throws IOException {
+    public static void generatorLP(int n, float distancias[][], float nodos[][]) throws IOException {
+        System.out.println(n + ";");
         //declaración de todas las variables que se usaran
-        String td, a, b, ts, xij, xji, u, h_b, M, pi, p;
-        td = a = b = ts = xij = xji = u = h_b = M = pi = p = "";
-        String t = "h" + n;//igualacion de t = la n-isma h
+        String td, a, b, ts, xij, xji, u, h_b, M, pi, p, x;
+        td = a = b = ts = xij = xji = u = h_b = M = pi = p = x = "";
+        String t = "t = h" + n + ";\n";//igualacion de t = la n-isma h
         int j_;//variable articial para manejar las restricciones del M grande
         for (int i = 0; i < n; i++) {
-            a += "a" + i + "=" + nodos[i][0] + ";\n"; //asignando ai
-            b += "b" + i + "=" + nodos[i][1] + ";\n"; //asignando bi
-            ts += "ts" + i + "=" + nodos[i][2] + ";\n"; //asignando tsi
+            a += "a" + i + "=" + nodos[i][1] + ";\n"; //asignando ai
+            b += "b" + i + "=" + nodos[i][2] + ";\n"; //asignando bi
+            ts += "ts" + i + "=" + nodos[i][0] + ";\n"; //asignando tsi
             h_b += "h" + i + " <= " + "b" + i + ";\n";//hi<=bi
             if (i != 0) {
                 p += "p" + i + " = a" + i + " - " + "h" + i + " + " + "s" + i + ";\n";//pi=ai-hi +si
@@ -104,11 +108,18 @@ public class Command {
                 pi += "p" + 0 + " = 0;\n";
             }
             for (int j = 0; j < n; j++) {
+                if (i == 0 && j == 0) {
+                    x += "x" + i + j;
+                } else {
+                    x += ",x" + i + j;
+                }
                 if (i != 0 && i != j) {
                     u += "u" + i + " - " + "u" + j + " + " + n + " x" + i + j + " <= n-1;\n";//ui - uj + n*Xij <= n-1
                 }
                 if (i != j) {
                     td += "td" + i + j + " = " + distancias[i][j] + "; \n";//tdij
+                    System.out.println(i + " " + j + " " + distancias[i][j] + ";");
+
                     j_ = (j == 0) ? n : j;//cambio para asegurar que del ultimo nodo se vaya al nodo inicial 
                     M += "h" + j_ + " - h" + i + " + 100000 - 100000 x" + i + j + " >= td" + i + j + " + " + "ts" + i + " + " + "p" + i + ";\n";//hj - hi +M -M*Xij >= tdij +tsi + pi
                 }
@@ -124,6 +135,7 @@ public class Command {
                     } else {//agregando el operador +
                         xij += " + x" + i + j;
                         xji += " + x" + j + i;
+
                     }
                 }
             }
@@ -132,9 +144,10 @@ public class Command {
             xji += " = 1;\n";
         }
         //concatenacion total para general el archivo lp
-        String lp = "min: t;\n" + "n =" + n + ";\n" + td + a + b + ts + t + xij + xji + u + h_b + M + p + pi;
+        String lp = "min: t;\n" + "n =" + n + ";\n" + td + a + b + ts + t + xij + xji + u + h_b + M + p + pi + "binary  " + x + ";";
+//        System.out.println(lp);
 
-        String ruta = "./tsptw.lp";
+        String ruta = "./out.lp";
         File archivo = new File(ruta);
         BufferedWriter bw;
 
@@ -148,52 +161,35 @@ public class Command {
 
         bw.close();
     }
+
+    public static void lp_solve(int n) throws IOException {
+        Process p = Runtime.getRuntime().exec("./lp_solve out.lp");
+
+        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+        int count = 0;
+        int limite = 2 * n * n + n + 6;
+        int inicio = n * n + 2 * n + 6;
+        String celdas[] = null;
+        String nodo;
+        String s;
+        while ((s = stdInput.readLine()) != null) {
+            //se elminan todos los espacios en blanco que envia lpsolve
+            s = s.replaceAll("\\s", "");
+            if (count == 4) {// numero de linea donde aparece la funcion objetivo
+                //Aquí se captura el valor de la función objetivo
+                System.out.println(s.substring(1) + ";");
+                count++;
+                continue;
+            }
+            if (count > inicio && count <= limite) {//intervalo en donde estan las variables Xij
+                //formato de llegada Xij{1|0}
+                //se realiza separacion de limite 4, se obtiene los siguientes caracteres
+                //[0] = X -- [1] = i -- [2] = j -- [3] = [1|0]
+                celdas = s.split("", 4);
+                System.out.println(celdas[1] + " " + celdas[2] + " " + celdas[3] + ";");
+            }
+            count++;
+        }
+    }
 }
-/**
- * para ejecutar el comando en linux
- */
-//        Process p = Runtime.getRuntime().exec("./lp_solve conventanasconespera.lp");
-//        BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
-//        while ((s = stdInput.readLine()) != null) {
-//            System.out.println(s + "\n");
-//        }
-/**
- * generar los array manualmente
- */
-//        // columnas tsi, ai, bi 
-//        int[][] nodo = {{2, 0, 0}, {3, 5, 22}, {6, 13, 22}, {1, 8, 22}};
-//        //columnas de td
-//        int distancias[][] = new int[4][4];
-//        distancias[0][1] = 1;
-//        distancias[0][2] = 1;
-//        distancias[0][3] = 1;
-//        distancias[1][0] = 5;
-//        distancias[1][2] = 2;
-//        distancias[1][3] = 5;
-//        distancias[2][0] = 6;
-//        distancias[2][1] = 3;
-//        distancias[2][3] = 2;
-//        distancias[3][0] = 5;
-//        distancias[3][1] = 4;
-//        distancias[3][2] = 3;
-/**
- * para imprimir los resultados
- */
-//mostrando por partes
-//        System.out.println("min: t;\n");
-//        System.out.println(td);
-//        System.out.println(a);
-//        System.out.println(b);
-//        System.out.println(ts);
-//        System.out.println("n =" + n + ";\n");
-//        System.out.println(t);
-//        System.out.println(xij);
-//        System.out.println(xji);
-//        System.out.println(u);
-//        System.out.println(h_b);
-//        System.out.println(M);
-//        System.out.println(p);
-//        System.out.println(pi);
-//mostrando total
-//        System.out.println(lp);
-        //Escribir el archivo
+
